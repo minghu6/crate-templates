1039c52fceb287d46c6a2e3fe1323e34cd7d211b
minghu6 <a19678zy@163.com>
2023-07-11 16:16:41 +0800

    update

--- leetcode/src/lib.rs
+++ leetcode/src/lib.rs
@@ -1,4 +1,23 @@
 
+#[macro_export]
+macro_rules! arr_to_vec_2 {
+    ([
+        $(
+            [$($e:expr),*]
+        ),*
+    ]) => {
+        vec![$(vec![$($e),*]),*]
+    };
+}
+
+
+#[macro_export]
+macro_rules! arrstr_to_vecstring {
+    ([$($e:expr),*]) => {
+        vec![$($e.to_string()),*]
+    };
+}
+
 
 #[cfg(test)]
 mod tests {
