2932b874b0b440fd6530cfe6c9665402453b7194
minghu6 <a19678zy@163.com>
2023-08-26 12:57:07 +0800

    update leetcdeo template

--- leetcode/src/lib.rs
+++ leetcode/src/lib.rs
@@ -1,57 +1,55 @@
 //!
 
-#[cfg(test)]
-mod tests {
-    #[macro_export]
-    macro_rules! arr_to_vec_2 {
-        ([
-            $(
-                [$($e:expr),*]
-            ),*
-        ]) => {
-            vec![$(vec![$($e),*]),*]
-        };
-    }
+macro_rules! arr_to_vec_2 {
+    ([
+        $(
+            [$($e:expr),*]
+        ),*
+    ]) => {
+        vec![$(vec![$($e),*]),*]
+    };
+}
 
-    #[macro_export]
-    macro_rules! arrstr_to_vecstring {
-        ([$($e:expr),*]) => {
-            vec![$($e.to_string()),*]
-        };
-    }
+macro_rules! arrstr_to_vecstring {
+    ([$($e:expr),*]) => {
+        vec![$($e.to_string()),*]
+    };
+}
 
-    macro_rules! set {
-        ($e:expr) => {
-            std::collections::BTreeSet::<Vec<i32>>::from_iter($e)
-        };
-    }
+macro_rules! set {
+    ($e:expr) => {
+        std::collections::BTreeSet::<Vec<i32>>::from_iter($e)
+    };
+}
 
-    #[macro_export]
-    macro_rules! min {
-        ($($val:expr),+) => {
-            {
-                [$($val),+].into_iter().min().unwrap()
-            }
-        };
-        ($($val:expr),+;$default:expr) => {
-            {
-                [$($val),+].into_iter().min().unwrap_or($default)
-            }
-        }
-    }
-
-    #[macro_export]
-    macro_rules! max {
-        ($($val:expr),+) => {
-            {
-                [$($val),+].into_iter().max().unwrap()
-            }
-        };
-        ($($val:expr),+;$default:expr) => {
-            {
-                [$($val),+].into_iter().max().unwrap_or($default)
-            }
+macro_rules! min {
+    ($($val:expr),+) => {
+        {
+            [$($val),+].into_iter().min().unwrap()
         }
     }
 }
 
+macro_rules! max {
+    ($($val:expr),+) => {
+        {
+            [$($val),+].into_iter().max().unwrap()
+        }
+    }
+}
+
+macro_rules s {
+    ($lit:literal) => {
+        $lit.to_string()
+    }
+}
+
+
+#[cfg(test)]
+mod tests {
+
+    #[test]
+    fn test() {
+
+    }
+}
